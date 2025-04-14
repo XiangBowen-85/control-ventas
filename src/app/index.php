c1053d51f1afa4b61bfd295061c65d586f93adaf
XiangBowen-85 <bowenx85@gmail.com>
2025-04-15 01:16:40 +0200

    feat: 15/4

--- src/app/index.php
+++ src/app/index.php
@@ -1,31 +1,20 @@
 <?php
-if (empty($_POST['usuario']) || empty($_POST['password'])) {
-    header('Location: ../');
+// TODO: Gestionar sesiones y usuarios
+/*if (empty($_POST['usuario']) || empty($_POST['password'])) {
+  header('Location: ../');
 }
 
-// region --- Conexión a la BBDD ---
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "control-ventas";
+include 'control-ventas.inc';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$sentencia = "SELECT * FROM usuarios WHERE email = ? AND password = SHA1(?)";
+$result = $conn->execute_query($sentencia, array($_POST['usuario'], $_POST['password']));
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-// endregion
-
-$sql = "SELECT * FROM `usuarios` WHERE `email` = '" . $_POST['usuario'] . "' AND `password` = '" . $_POST['password'] . "'";
-$result = $conn->query($sql);
-
-if ($result->num_rows == 0) {
-    header('Location: ../');
+if ($result->num_rows != 1) {
+  header('Location: ../');
 }
 
-$userdata = $result->fetch_assoc();
+$userdata = $result->fetch_assoc();*/
+
 ?>
 <!doctype html>
 <html lang="es">
@@ -40,10 +29,7 @@ $userdata = $result->fetch_assoc();
 </head>
 <body>
 
-<header>
-    <img src="https://placehold.co/100x70" alt="Logotipo">
-    <div>Bienvenido <?php echo $userdata['nombre'] . " " . $userdata['apellidos']; ?></div>
-</header>
+<?php include 'includes/menu.inc'; ?>
 
 <div class="contenedor">
     <?php
